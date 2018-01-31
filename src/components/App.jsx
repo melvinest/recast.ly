6275c6ebd6eb91487842f2bce0ba2e88c952bbe3
melvinest <melvinlouie.estrada@gmail.com>
2018-01-31 15:42:08 -0800

    Rendering all the views with static state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -27,21 +27,22 @@ class App extends React.Component {
   render() {
     return (
       <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <Search />
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <Search />
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <VideoPlayer video={this.props.videos[0]} />
+          </div>
+          <div className="col-md-5">
+            <VideoList videos={this.props.videos} />
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <VideoPlayer />
-      </div>
-      <div className="col-md-5">
-        <VideoList />
-      </div>
-    </div>
-  </div>
-      );
+      
+    );
   }
 }
 window.App = App;
